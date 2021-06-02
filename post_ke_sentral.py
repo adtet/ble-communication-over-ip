330c06c93c6fbab88e2757cb2529328bf329a4c6
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-06-03 00:54:41 +0700

    adit commit data ke sentral

--- post_ke_sentral.py
+++ post_ke_sentral.py
@@ -1,5 +1,13 @@
-from sqllib
+from sqllib import ambil_data
 import requests
+
+url = "http://ipraspberry:port/path" # sesuai rasberry
+data =  ambil_data()
+
+for i in range(0,len(data)):
+    x =  requests.post(url=url,data=data[i])
+    print(x.text)
+
 
 
 
